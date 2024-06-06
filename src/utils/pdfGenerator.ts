36a6e777fab2d0fa7225bf9a14565d986775ad75
ToneseekerMusical <105748910+ToneseekerMusical@users.noreply.github.com>
2024-06-05 22:29:30 -0400

    added expected typescript error comment

--- src/utils/pdfGenerator.ts
+++ src/utils/pdfGenerator.ts
@@ -1,5 +1,6 @@
 import getTemplate from "./pdfTemplates/pdfTemplateQuery";
 
 export default async function pdfGenerator(fields: { [path: string]: unknown; }): Promise<void> {
+  //@ts-expect-error
   const template = await getTemplate(fields.pdfOptions?.pdfTemplate)
 }
